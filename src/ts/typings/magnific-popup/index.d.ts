0ad7bfc95289c66d04851ab6b19a472468876fdd
K.yamazaki <saxeblue2012@gmail.com>
2019-07-29 16:24:58 +0900

    magnific-popupを追加

---
+++ src/ts/typings/magnific-popup/index.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="jquery" />
+
+interface JQuery {
+  magnificPopup(opts?:any):JQuery;
+}
+
+interface JQueryStatic {
+  magnificPopup: JQueryMagnificPopupStatic;
+}
+
+interface JQueryMagnificPopupStatic {
+  open: any;
+  (): JQuery;
+  parameter(name: string): string;
+  parameter(name: string, value: string, append?: boolean): JQuery;
+}
