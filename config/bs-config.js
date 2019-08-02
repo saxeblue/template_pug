ba6ec2f60c400dd46d76ea38edc773aa648754f1
K.yamazaki <saxeblue2012@gmail.com>
2019-08-03 01:38:46 +0900

    bs-configを修正

--- config/bs-config.js
+++ config/bs-config.js
@@ -1,18 +1,19 @@
 
 module.exports = {
     
-    ui: {
-        port: 3001,
-        weinre: {
-            port: 8080
-        }
-    },
-    port: 3000,
-    server: {
-        baseDir: './htdocs'
-    },
-    files: './htdocs/**/*',
-    tunnel: false,
-    notify: false,
-    online: true
+  ui: {
+    port: 3001,
+    weinre: {
+      port: 8080
+    }
+  },
+  port: 3000,
+  server: {
+    baseDir: './htdocs'
+  },
+  files: './htdocs/**/*',
+  open: 'external',
+  tunnel: false,
+  notify: false,
+  online: true
 };
